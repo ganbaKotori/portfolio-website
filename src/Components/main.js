da2e9a93a3109aa49c195566d17bb39394063c24
Alex Ramirez <alexanderjamesramirez@gmail.com>
2019-05-07 01:11:01 -0700

    fixed main

--- src/Components/main.js
+++ src/Components/main.js
@@ -6,16 +6,16 @@ import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
-``
+
 
 const Main = () => (
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/aboutme" component={AboutMe} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/resume" component={Resume} />
-    </Switch>
-  )
-  
-  export default Main;
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={AboutMe} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
+)
+
+export default Main;
