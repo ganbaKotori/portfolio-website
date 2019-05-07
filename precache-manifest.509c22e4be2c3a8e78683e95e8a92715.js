cb0524f99c2398262c80ef25e881182058d0d3af
Alex Ramirez <alexanderjamesramirez@gmail.com>
2019-05-07 01:16:27 -0700

    Updates

---
+++ precache-manifest.509c22e4be2c3a8e78683e95e8a92715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e73fba6a44e5d421b4fb8104aa7f68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fe5cd251619e4b72bcc",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "d5ee445b1ef068b539c1",
+    "url": "/static/css/main.59521e65.chunk.css"
+  },
+  {
+    "revision": "6fe5cd251619e4b72bcc",
+    "url": "/static/js/2.7004423a.chunk.js"
+  },
+  {
+    "revision": "d5ee445b1ef068b539c1",
+    "url": "/static/js/main.9f9c2f62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
