7623303a4607ffb715121ce02dccd85dae2335c1
Alex Ramirez <alexanderjamesramirez@gmail.com>
2019-05-06 20:37:55 -0700

    Updates

--- precache-manifest.dc7353e5bddf87f3c95f2981e25cbef8.js
+++ precache-manifest.dc7353e5bddf87f3c95f2981e25cbef8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1cb0bd9f1f6c4b5e126c2b3165cee72a",
+    "revision": "6f022a3e2251f41231e2c4bf3bc196d2",
     "url": "/portfolio-website/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-website/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "5abab2fc93a19e0c38fc",
+    "revision": "4c4bb00f0e20022eef47",
     "url": "/portfolio-website/static/css/main.59521e65.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-website/static/js/2.e82c7dd5.chunk.js"
   },
   {
-    "revision": "5abab2fc93a19e0c38fc",
-    "url": "/portfolio-website/static/js/main.41e4a2fc.chunk.js"
+    "revision": "4c4bb00f0e20022eef47",
+    "url": "/portfolio-website/static/js/main.1ae1d8e0.chunk.js"
   },
   {
     "revision": "0e1e731819752b5e1982",
